f1525ab4c33d5fd8351d56ee427925f1204fbb91
azimezn <117550865+azimezn@users.noreply.github.com>
2023-11-22 15:23:08 -0500

    got a working react structure

---
+++ client/src/component/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>hello</h1>
+            <p>hi there</p>
+        </header>
+    )
+}
+
+export default Header
