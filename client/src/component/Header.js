89aa8c7438389cef1c74a8ef915cffd2fee543b5
azimezn <117550865+azimezn@users.noreply.github.com>
2023-11-22 16:43:00 -0500

    fixed more. changed colors and centered. just basics, all will change

--- client/src/component/Header.js
+++ client/src/component/Header.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 
-const pages = ["Home", "Community", "Renovation"];
+const pages = ["Home", "Community", "Renovation", "Showroom", "Cabinets", "Countertops", "LVP", "Tile", "Hardware", "SALE", "Blog", "Financing", "Contact"];
 
 const Header = ({ currentPage, setCurrentPage }) => {
     return (
         <header>
-            <h1>Design & Remodel Brothers</h1>
+            <h1>DESIGN & REMODEL BROTHERS</h1>
+            <p>YOUR HOME, REIMAGINED</p>
             <Nav className="justify-content-center" activeKey={currentPage}>
                 {/* maps through the pages and lists them */}
                 {pages.map(item => (
                     <Nav.Item key={item}>
                         <Nav.Link onClick={() => setCurrentPage(item)}
                             style={{
-                                color: currentPage === item ? "#f9f1ec" : "transparent"
+                                color: currentPage === item ? "gray" : "transparent"
                             }}
                             className="nav-link">
                             {item}
