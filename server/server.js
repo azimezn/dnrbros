0e0d82bf8abe9e177a386c2014c3ef85f6a0f938
azimezn <117550865+azimezn@users.noreply.github.com>
2023-11-22 15:02:03 -0500

    decided to use react. changed structure

--- server/server.js
+++ server/server.js
@@ -6,6 +6,9 @@ const routes = require('./routes')
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// serving static files from the public directory
+app.use(express.static('public'));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
